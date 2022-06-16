da00426759755c49149fe3e898bf9db948addaa0
Miguel del Mazo <migueldelmazo@gmail.com>
2022-06-16 18:08:19 +0200

    Added projectName to collect

--- post-commit.js
+++ post-commit.js
@@ -1,24 +1,22 @@
-const axios = require('axios')
 const utils = require('./utils')
-
-// check account id in package.json
-
 const packageConfig = utils.getPackageConfig()
-if (!packageConfig.proyectName) {
-  utils.log('we needs package.json > yeeeha > proyectName')
+
+// check projectName in package.json
+if (!packageConfig.projectName) {
+  utils.log('we needs package.json > yeeeha > projectName')
   return
 }
 
 // get last commit info
-
 const commitInfo = utils.execute('git log -1')
 const commitHash = utils.getCommitHash(commitInfo)
 const commitAuthor = utils.getCommitAuthor(commitInfo)
 const commitDate = utils.getCommitDate(commitInfo)
-utils.log(commitHash)
-utils.log(commitAuthor)
-utils.log(commitDate)
 
 // send data to GA
-
-utils.collet({ commitHash, commitAuthor, commitDate })
+utils.collet({
+  commitHash,
+  commitAuthor,
+  commitDate,
+  projectName: packageConfig.projectName
+})
